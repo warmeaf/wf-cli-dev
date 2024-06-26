846265f79c551a261ce27eae9abba67b4541d433
nextc <1391040917@qq.com>
2024-06-26 09:27:43 +0800

    feat: 实现检查最低 node 版本功能的开发

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,16 +1,41 @@
 'use strict'
 
 module.exports = core
+
+const semver = require('semver')
+const colors = require('colors/safe')
 // require: .js/.json/.node
 // any -> .js
 const pkg = require('../package.json')
 const log = require('@wf-cli-dev/log')
+const constant = require('./const')
 
 function core() {
-  checkVersion()
+  // try catch 包裹，不显示不必要的报错堆栈信息
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (e) {
+    log.error(e.message)
+  }
 }
 
-// 检查版本号
-function checkVersion() {
-  log.notice('wf-cli', pkg.version)
+// 检查包版本号
+function checkPkgVersion() {
+  log.notice('wf-cli 版本', pkg.version)
+}
+
+// 检查node版本号
+function checkNodeVersion() {
+  // 1. 获取当前node版本号
+  const currentVersion = process.version
+  // 2. 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(
+      colors.red(
+        `wf-cli 需要安装 v${constant.LOWEST_NODE_VERSION} 以上版本的 node`
+      )
+    )
+  }
 }
